84678e8a5684e9a84342a8871215cb0c530baa6f
Katie <Katie@KatiesPC>
2021-04-25 20:02:45 +1000

    Added few lines while testing

--- PlyFiles/plytest.m
+++ PlyFiles/plytest.m
@@ -3,7 +3,7 @@ function Environment()
 %% Place Objects in Environment
 % Testing the look of ply files in Matlab simulation
 % at first just with placeobject
-
+hold on
 % Placing Board
 PlaceObject('boardv2.ply', [0, 0, 0]);
 
@@ -14,7 +14,7 @@ PlaceObject('Rtile.ply',[2,0,0]);
 PlaceObject('Rtile.ply',[2,1.8,0]);
 
 % Placing Blue tiles example start positions
-PlaceObject('Rtile.ply',[1,-1.1,1]);
-PlaceObject('Rtile.ply',[-1,-1.1,1]);
-PlaceObject('Rtile.ply',[2,2,2]);
-PlaceObject('Rtile.ply',[2,1.8,2]);
+PlaceObject('Btile.ply',[1,-1.1,1]);
+PlaceObject('Btile.ply',[-1,-1.1,1]);
+PlaceObject('Btile.ply',[2,2,2]);
+PlaceObject('Btile.ply',[2,1.8,2]);
