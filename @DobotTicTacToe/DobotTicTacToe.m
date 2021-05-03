bed9645b25dfe211c8c0848b248f3e6e55d7639f
Luke <modularluke@gmail.com>
2021-05-03 14:54:17 +1000

    skeleton for dobot tic tac toe game class

---
+++ @DobotTicTacToe/DobotTicTacToe.m
@@ -0,0 +1,40 @@
+classdef DobotTicTacToe < handle
+% class to control dobot to play tic tac toe with a human
+properties
+    game = TicTacToe();
+    dobot = DobotMagician();
+    cam; % webcam
+end
+
+methods
+function self = DobotTicTacToe()
+    self.cam = webcam(2);
+end
+
+function board = SenseBoardState(self)
+    % look at which colour occupies which board tile
+    
+    % return board state
+end
+
+function obstruction = SenseObstruction(self,referenceimage)
+    % compare reference image and current webcam output
+    
+    % if sum is significantly different, there is an obstruction
+end
+
+function move = FindPlayerMove(self)
+    newboard = SenseBoardState(self);
+    
+    % compare new board and stored board
+    
+end
+
+function image = TakeImage(self)
+    % take image from webcam
+    image = snapshot(self.cam);
+end
+
+end
+end
+
