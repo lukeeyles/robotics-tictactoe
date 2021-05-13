775a691727031120b1379c9c73e4442cbb567639
Luke <modularluke@gmail.com>
2021-05-13 11:32:26 +1000

    take q as input and return new q

--- @DobotTicTacToe/manual.m
+++ @DobotTicTacToe/manual.m
@@ -1,18 +1,12 @@
-function manual (self, v, gain, steps)
+function newq = manual (self, v, oldq, gain)
 manual_mode = 1; % turn on manual movement
 robot = self.dobot;
 
-%startpos = transl(0.1, 0.1, 0.1);
-%qstart = robot.Ikine(startpos);
-%robot.Plot(qstart);
-
-% find current joint positions
-oldq = robot.GetPos();
+% find current position
 oldpos = robot.Fkine(oldq);
 
 % translate proportional to velocity given
 newpos = oldpos*transl(gain*v);
-q2 = robot.Ikine(newpos);
-qMatrix = jtraj(oldq,q2, steps);
-robot.Animate(qMatrix);
+newq = robot.Ikine(newpos);
+robot.Animate(newq);
 end
