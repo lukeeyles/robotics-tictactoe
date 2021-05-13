701166e3c7f9d1b2ba34ea846e37cb76d7cff2d1
Katie <Katie@172.19.115.198>
2021-05-13 13:26:52 +1000

    Start collision avoidance for stationary obstacle

---
+++ collisionavoidance.m
@@ -0,0 +1,14 @@
+% collision avoidance
+    % Move around/avoid cube obstacle
+    
+%start pose
+
+%end pose
+
+%cube location
+
+%possible waypoints
+
+%check collision within each traj with a possible waypoint
+
+%return the successful one
