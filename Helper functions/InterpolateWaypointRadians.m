7caac626abd66b1bfbffe0fa99c26119f8bc155e
Katie <Katie@KatiesPC>
2021-05-16 18:11:55 +1000

    adding helper functions for updated collision avoidance

---
+++ Helper functions/InterpolateWaypointRadians.m
@@ -0,0 +1,13 @@
+%% InterpolateWaypointRadians
+% Borrowed from Lab5
+% Given a set of waypoints, finely intepolate them
+function qMatrix = InterpolateWaypointRadians(waypointRadians,maxStepRadians)
+if nargin < 2
+    maxStepRadians = deg2rad(1);
+end
+
+qMatrix = [];
+for i = 1: size(waypointRadians,1)-1
+    qMatrix = [qMatrix ; FineInterpolation(waypointRadians(i,:),waypointRadians(i+1,:),maxStepRadians)]; %#ok<AGROW>
+end
+end
