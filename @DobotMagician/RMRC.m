792a5df4115dd34a248bf66eeafe4c77a02852e0
Luke <modularluke@gmail.com>
2021-05-16 18:54:20 +1000

    do rmrc on the dobot

---
+++ @DobotMagician/RMRC.m
@@ -0,0 +1,17 @@
+function qReal = RMRC(self,v,qReal,dt)
+% v is vx, vy, vz
+J = self.Jacob0(qReal);
+manipulability = sqrt(abs(det(J*J')));
+threshold = 0.002;
+lambdamax = 0.1;
+lambda = 0;
+if manipulability < threshold
+    disp("applying singularity avoidance");
+    lambda = (1 - (manipulability/threshold)^2)*lambdamax;
+end
+JDLSinv = inv(J'*J + lambda*eye(3))*J';
+qdot = JDLSinv*v';
+
+% qdot = inv(J)*v';
+qModel = self.qRealToModel(qReal) + (dt*[qdot;0;0])';
+qReal = self.qModelToReal(qModel);
