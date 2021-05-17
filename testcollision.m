bdefe8e7689f7b34f03e815d5f7825f04cade24a
Luke <modularluke@gmail.com>
2021-05-17 14:55:43 +1000

    test script for collisions

--- testcollision.m
+++ testcollision.m
@@ -1,5 +1,5 @@
 cx = 0.2;
-cy = -0.09;
+cy = 0.09;
 cz = -0.09;
 centerpnt = [cx,cy,cz];
 side = 0.1;
@@ -10,31 +10,37 @@ hold on;
 r.Plot(r.qn);
 
 %% avoid collision
-goal = transl(0,-0.2,0);
+goal = transl(0,0.2,0);
 q1 = r.qn;
 q2 = r.Ikine(goal);
+r.Animate(q1);
 [qWaypoints] = CollisionAvoidances(q1,q2,r,faces,vertex,faceNormals);
-for i = 2:size(qWaypoints,1)
-    path = jtraj(qWaypoints(i-1,:),qWaypoints(i,:),30);
-    r.Animate(path);
+path = [q1;qWaypoints];
+for i = 2:size(path,1)
+    qMatrix = jtraj(path(i-1,:),path(i,:),30);
+    r.Animate(qMatrix);
 end
 
 %% can't avoid collision
-goal = transl(0.2,-0.1,-0.1);
+goal = transl(0.2,0.1,-0.1);
 q1 = r.qn;
-q2 = r.Ikine(goal)
+q2 = r.Ikine(goal);
+r.Animate(q1);
 [qWaypoints] = CollisionAvoidances(q1,q2,r,faces,vertex,faceNormals);
-for i = 2:size(qWaypoints,1)
-    path = jtraj(qWaypoints(i-1,:),qWaypoints(i,:),30);
-    r.Animate(path);
+path = [q1;qWaypoints];
+for i = 2:size(path,1)
+    qMatrix = jtraj(path(i-1,:),path(i,:),30);
+    r.Animate(qMatrix);
 end
 
 %% no collision
-goal = transl(0.2,0,-0.1);
+goal = transl(0.2,-0.1,0);
 q1 = r.qn;
-q2 = r.Ikine(goal)
+q2 = r.Ikine(goal);
+r.Animate(q1);
 [qWaypoints] = CollisionAvoidances(q1,q2,r,faces,vertex,faceNormals);
-for i = 2:size(qWaypoints,1)
-    path = jtraj(qWaypoints(i-1,:),qWaypoints(i,:),30);
-    r.Animate(path);
+path = [q1;qWaypoints];
+for i = 2:size(path,1)
+    qMatrix = jtraj(path(i-1,:),path(i,:),30);
+    r.Animate(qMatrix);
 end
